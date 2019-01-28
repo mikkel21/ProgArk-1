90b7b6bce1b7628bdf4cc20859bfd521778df809
Mikkel Benjamin Lerberg Nygard <mbnygard@stud.ntnu.no>
2019-01-28 17:22:04 +0100

    4 helis moving randomly without collision detection

--- core/src/com/mygdx/game/states/PlayState.java
+++ core/src/com/mygdx/game/states/PlayState.java
@@ -6,13 +6,18 @@ import com.mygdx.game.Exercise_1;
 import com.mygdx.game.sprites.Heli;
 
 public class PlayState extends State {
-    private Heli heli;
-    private BitmapFont font;
+    private Heli heli1;
+    private Heli heli2;
+    private Heli heli3;
+    private Heli heli4;
+
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
-        heli = new Heli(150,150);
-        font = new BitmapFont();
+        heli1 = new Heli(20,20);
+        heli2 = new Heli(Exercise_1.WIDTH-40,40);
+        heli3 = new Heli(Exercise_1.WIDTH-40,600);
+        heli4 = new Heli(20,600);
     }
 
     protected void handleInput() {
@@ -22,14 +27,23 @@ public class PlayState extends State {
     @Override
     public void update(float dt) {
         handleInput();
-        heli.update(dt);
+        heli1.update(dt);
+        heli2.update(dt);
+        heli3.update(dt);
+        heli4.update(dt);
     }
 
     @Override
     public void render(SpriteBatch sb) {
         sb.begin();
-        sb.draw(heli.getTexture(),heli.getPosition().x, heli.getPosition().y);
-        font.draw(sb,"x,y: " + heli.getPosition().x + "," + heli.getPosition().y,20,20);
+        sb.draw(heli1.getTexture(),heli1.getPosition().x, heli1.getPosition().y);
+
+        sb.draw(heli2.getTexture(),heli2.getPosition().x, heli2.getPosition().y);
+
+        sb.draw(heli3.getTexture(),heli3.getPosition().x, heli3.getPosition().y);
+
+        sb.draw(heli4.getTexture(),heli4.getPosition().x, heli4.getPosition().y);
+
         sb.end();
     }
 
